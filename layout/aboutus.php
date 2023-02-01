9620ddef1327a6a967240fa3ccef210cf4b02f8b
TeeniJai <jenina.tiainen@gmail.com>
2023-02-01 10:55:50 +0200

    pain

--- layout/aboutus.php
+++ layout/aboutus.php
@@ -3,5 +3,5 @@ $title = "about us: HAMK OY";
 include "header.php" ?>
 <h2>Contact us</h2>
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut aperiam animi porro libero obcaecati, totam, commodi quaerat magni esse atque magnam repellat delectus architecto officiis laudantium recusandae excepturi deleniti cum?</p>
-
+<p>did u know this is fkn annoying when u change ONE (1) file's location</p>
 <?php include "footer.php" ?>
