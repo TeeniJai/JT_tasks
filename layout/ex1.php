15f4adad1b458aa4c1e45e2538188b8273fc676a
TeeniJai <jenina.tiainen@gmail.com>
2023-02-01 11:29:59 +0200

    fix

--- layout/ex1.php
+++ layout/ex1.php
@@ -1,4 +1,4 @@
-<?php $title = "Indivitual tasks 1"
+<?php $title = "Indivitual tasks 1";
 include "header.php" ?>
     <h3>Write a simple PHP script to print your information (Name and your groupid).</h3>
 <?php
