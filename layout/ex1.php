8d9cf00758d5956406e0b8f2b14b88fe190f4ef7
TeeniJai <jenina.tiainen@gmail.com>
2023-02-01 11:25:50 +0200

    more tasks

--- layout/ex1.php
+++ layout/ex1.php
@@ -1,17 +1,45 @@
 <?php include "header.php" ?>
-    <h1>Write a simple PHP script to print your information (Name and your groupid).</h1>
+    <h3>Write a simple PHP script to print your information (Name and your groupid).</h3>
 <?php
 echo "Jenina Tiainen <br>";
 echo "BBCAP22"
 ?> 
-<h1>Write PHP code to display the following message.</h1>
+<h3>Write PHP code to display the following message.</h3>
 <?php
 echo "Hello world! My name is David"
 ?>
-<h1>Write the PHP code in to display the current date.</h1>
+<h3>Write the PHP code in to display the current date.</h3>
 <?php
 echo date("d.m.Y");
 ?>  
+<h3>3.4 variable in php</h3>
+<?php
+$title1 = "PHP is interesting";
+echo "<h1>". $title1. "</h1>";
+?>
+
+<h3>3.5 Table & Variables</h3>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 3;
+echo "
+<table>
+<tr>
+<th> S.N </th><th> Name </th><th> Grade </th>
+</tr>
+<tr>
+<td> 1 </td><td> Pekka </td><td> $g1 </td>
+</tr>
+<td> 2 </td><td> Johanna </td><td> $g2 </td>
+</tr>
+<td> 3 </td><td> John </td><td> $g3 </td>
+</tr>
+</table>
+";
+?>
+<h3> 4 screenshot of the depelopment environment</h3>
+<img src="dockerSS.png" alt="screenshot of docker">
 
 
 <?php include "footer.php" ?>
