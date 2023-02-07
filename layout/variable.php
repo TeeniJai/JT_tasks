618761d4cbb2f4812f161ec2bfc85e4a8019f666
TeeniJai <jenina.tiainen@gmail.com>
2023-02-07 09:30:52 +0200

    forms

---
+++ layout/variable.php
@@ -0,0 +1,17 @@
+<?php include "header.php" ?>
+
+<h3> Exercise 3. In-class Task Variable & Operators</h3>
+
+<h2>Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using h3
+    tag: Hello …., You are welcome to my site.
+</h2>
+<form>
+
+First name: <imput type="text" name="fname"><br>
+Last name: <imput type="text" name="fname"><br>
+Birth date <imput type="date" name="bdate"><br>
+Select your favourite color <imput type="color" name="color">
+<imput type="submit" value="Submit">
+
+</form>
+<?php include "footer.php" ?>
